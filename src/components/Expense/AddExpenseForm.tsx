997c5271d2323a8c52eec401f6db3322b7f53d9a
panda2k <thelowkeypanda@gmail.com>
2024-10-24 10:48:33 -0700

    Done

--- src/components/Expense/AddExpenseForm.tsx
+++ src/components/Expense/AddExpenseForm.tsx
@@ -1,48 +1,59 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+
+const randomId = () => {
+    return `${Math.random() * 10000}`
+}
+
 const AddExpenseForm = () => {
-  // Exercise: Consume the AppContext here
+    // Exercise: Consume the AppContext here
+    const { setExpenses, expenses } = useContext(AppContext)
+    // Exercise: Create name and cost to state variables
+    const [name, setName] = useState<string>("");
+    const [cost, setCost] = useState<number>(0);
 
-  // Exercise: Create name and cost to state variables
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-    // Exercise: Add add new expense to expenses context array
-  };
+        // Exercise: Add add new expense to expenses context array
+        setExpenses([...expenses, { name, cost, id: randomId() }])
+    };
 
-  return (
-    <form onSubmit={(event) => onSubmit(event)}>
-      <div className="row">
-        <div className="col-sm">
-          <label htmlFor="name">Name</label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="name"
-            value={""}
-            // HINT: onChange={}
-          ></input>
-        </div>
-        <div className="col-sm">
-          <label htmlFor="cost">Cost</label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="cost"
-            value={0}
-            // HINT: onChange={}
-          ></input>
-        </div>
-        <div className="col-sm">
-          <button type="submit" className="btn btn-primary mt-3">
-            Save
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+    return (
+        <form onSubmit={(event) => onSubmit(event)}>
+            <div className="row">
+                <div className="col-sm">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        required
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        data-testid="name-input"
+                    ></input>
+                </div>
+                <div className="col-sm">
+                    <label htmlFor="cost">Cost</label>
+                    <input
+                        required
+                        type="text"
+                        className="form-control"
+                        id="cost"
+                        onChange={(e) => setCost(Number(e.target.value))}
+                        data-testid="cost-input"
+                    ></input>
+                </div>
+                <div className="col-sm">
+                    <button type="submit" className="btn btn-primary mt-3" data-testid="save-button">
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
 };
 
 export default AddExpenseForm;
